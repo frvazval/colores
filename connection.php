47632a4f3ac95bf15dd382c64a81f0d1931d6034
Fvazquez <foapgenerico@365.stucom.com>
2025-05-08 19:19:42 +0200

    stucom

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+// Datos de acceso a la BD
+// tambien se puede poner $server = "127.0.0.1";
+$server = "localhost";
+$database = "colores";
+$port = 3307;
+$user = "root";
+$password = "root";
+
+?>
