5e4b8b483e494006bfd68cfcb6a8ae287fae833f
Fvazquez <foapgenerico@365.stucom.com>
2025-05-08 19:56:37 +0200

    stucom

--- connection.php
+++ connection.php
@@ -2,7 +2,8 @@
 // Datos de acceso a la BD
 // tambien se puede poner $server = "127.0.0.1";
 $host = "localhost";
-$database = "colores";
+// $database = "colores";
+$database = "biblioteca";
 $port = 3307;
 $user = "root";
 $password = "root";
@@ -10,7 +11,12 @@ $password = "root";
 // Conexión a la base de datos
 try {
     $conn = new PDO ("mysql:host=$host;port=$port;dbname=$database", $user, $password);
-    echo "Conectado !!";
+    // echo "Conectado !!";
+    foreach ($conn -> query("SELECT * FROM usuarios") as $fila) {
+        echo "<pre>";
+        print_r ($fila);
+        echo "</pre>";
+    }
 } catch (PDOException $e) {
     echo $e->getMessage();
 }
