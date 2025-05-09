c4c28edb94fdaa234651bfa02251f6a9e8c2ed46
Fvazquez <foapgenerico@365.stucom.com>
2025-05-09 18:33:23 +0200

    stucom

--- index.php
+++ index.php
@@ -32,7 +32,8 @@ $array_filas = $select_pre->fetchAll();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Colores</title>
+    <title>Colores</title>   
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
@@ -49,6 +50,10 @@ $array_filas = $select_pre->fetchAll();
                 ?>
                 <div style="background-color: <?=$fila['color_en'] ?>;color:<?=$color?>;">
                     <p> <?php echo $fila["usuario"] ?></p>
+                    <span>
+                        <i class="fa-regular fa-pen-to-square"></i>
+                        <i class="fa-solid fa-trash-can"></i>
+                    </span>
                 </div>
 
             <?php endforeach ?>
