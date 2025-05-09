704ccbc9bcb696ed75d422b59da7372825edb3f9
Fvazquez <foapgenerico@365.stucom.com>
2025-05-09 15:47:44 +0200

    stucom

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
-echo "soy index.php";
+// echo "soy index.php";
 // Con el include no se generaria un error critico si no se conecta, solo un warning
-include "connection.php";
+// include "connection.php";
 // Hace el include solo una vez
-include_once "connection.php";
+// include_once "connection.php";
 // Con el require se generaria un error critico si no se conecta, y no se ejecutaria el resto del codigo
-require "connection.php";
+// require "connection.php";
 // Hace el require solo una vez
 require_once "connection.php";
 
