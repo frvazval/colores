80c41b416881e05838215afcb66f11a2a4fa844c
Fvazquez <foapgenerico@365.stucom.com>
2025-05-09 17:53:32 +0200

    stucom

--- insert.php
+++ insert.php
@@ -1,3 +1,35 @@
 <?php
 
-echo "Soy insert.php";
+// Llama a la conexion una vez
+require_once "connection.php";
+require_once "traduccion_colores.php";
+
+// $_POST
+// echo "<pre>";
+// print_r($_POST);
+// echo "</pre>";
+
+$usuario = $_POST["usuario"];
+$color_es = strtolower($_POST["color"]);
+
+$encontrado = false;
+foreach ($array_colores_es_en as $clave => $valor) {
+    if ($clave == $color_es) {
+        $encontrado = true;
+        break;
+    }
+}
+
+if (!$encontrado) {
+    $color_es = "blanco";
+}
+
+// 1 - Definir la sentencia preparada
+$insert = "INSERT INTO colores(usuario, color_es, color_en) VALUES (?, ?, ?);";
+// 2 - Preparar la sentencia
+$insert_pre = $conn->prepare($insert);
+// 3 - Ejecutar la sentencia
+$insert_pre->execute([$usuario, $color_es, $array_colores_es_en[$color_es]]);
+
+// para que vuelva a la pagina principal
+header('location: index.php');
---
+++ traduccion_colores.php
@@ -0,0 +1,14 @@
+<?php
+
+$array_colores_es_en = [
+    "rojo" => "red",
+    "verde" => "green",
+    "azul" => "blue",
+    "amarillo" => "yellow",
+    "naranja" => "orange",
+    "morado" => "purple",
+    "rosa" => "pink",
+    "negro" => "black",
+    "salmon oscuro" => "darksalmon",
+    "blanco" => "white"
+];
