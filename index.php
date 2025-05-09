6e94ba7b0ebb53aba2faee8d3b00298e55ff9869
Fvazquez <foapgenerico@365.stucom.com>
2025-05-09 16:32:16 +0200

    stucom

--- index.php
+++ index.php
@@ -9,5 +9,47 @@
 // Hace el require solo una vez
 require_once "connection.php";
 
-
+// 1 - Definir la sentencia (query)
+$select = "SELECT * FROM colores;";
+// 2 - Preparar la sentencia
+$select_pre = $conn->prepare($select);
+// 3 - Ejecutar la sentencia
+$select_pre->execute();
+// 4 - Obtener los resultados
+$array_filas = $select_pre->fetchAll();
+// 5 - Mostrar los resultados
+// foreach ($array_filas as $fila) {
+//     echo "<pre>";
+//     print_r($fila);
+//     echo "</pre>";
+// }
 ?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Colores</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <header><h1>Nuestros colores preferidos</h1></header>
+    <main>
+        <section>
+            <h2>Nuestros usuarios</h2>
+            <?php foreach($array_filas as $fila) : ?>
+                <div style="background-color: <?=$fila['color_en'] ?>;">
+                    <p> <?php echo $fila["usuario"] ?></p>
+                </div>
+
+            <?php endforeach ?>
+        </section>
+        <section>
+            <h2>Indica tus datos</h2>
+
+        </section>    
+
+    </main>
+</body>
+</html>
