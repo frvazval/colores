c8ed5f5d61c3daca058739680cf1956ba4d3f3cd
Fvazquez <foapgenerico@365.stucom.com>
2025-05-13 15:08:58 +0200

    stucom

--- update.php
+++ update.php
@@ -8,7 +8,7 @@ $update = "update FROM colores SET usuario = ?, color_es = ?, color_en = ? WHERE
 // 2 - Preparar la sentencia
 $update_pre = $conn->prepare($update);
 // 3 - Ejecutar la sentencia
-$update_pre->execute([$_GET['id']]);
+$update_pre->execute([$usuario, $color_es, $array_colores_es_en[$color_es],$_GET['id']]);
 // 4 - Anular el $update_pre
 $update_pre = null;
 // 5 - Anular el $conn
