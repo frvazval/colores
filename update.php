608be9538133ec8eeaad215656d39e31e5da69d3
Fvazquez <foapgenerico@365.stucom.com>
2025-05-13 15:55:10 +0200

    stucom

--- update.php
+++ update.php
@@ -2,13 +2,19 @@
 
 // Llama a la conexion una vez
 require_once "connection.php";
+require_once "traduccion_colores.php";
+
+$usuario = $_POST['usuario'];
+$color_es = $_POST['color'];
+$color_en = $array_colores_es_en[$color_es];
+$id_color = $_POST['id_color'];
 
 // 1 - Definir la sentencia preparada
 $update = "update FROM colores SET usuario = ?, color_es = ?, color_en = ? WHERE id_color = ?;";
 // 2 - Preparar la sentencia
 $update_pre = $conn->prepare($update);
 // 3 - Ejecutar la sentencia
-$update_pre->execute([$usuario, $color, $array_colores_es_en[$color], $_POST['id_color']]);
+$update_pre->execute($usuario, $color_es, $color_en, $id_color);
 // 4 - Anular el $update_pre
 $update_pre = null;
 // 5 - Anular el $conn
