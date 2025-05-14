c41c231690c1f333474f92412d9bd321326c256d
Fvazquez <foapgenerico@365.stucom.com>
2025-05-14 15:50:34 +0200

    stucom

---
+++ crear_usuario.php
@@ -0,0 +1,13 @@
+
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <?php include_once "modulos/meta.php"; ?>
+    <title>Crear cuenta</title>
+</head>
+<body>
+     <?php include_once "modulos/header.php"; ?>
+</body>
+</html>
---
+++ modulos/meta.php
@@ -0,0 +1,7 @@
+<   
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="keywords" content="Colores, Colors, Colours">
+<meta name="author" content="Paco">
+<meta name="description" content="Colores favoritos de nuestros amigos">
+
